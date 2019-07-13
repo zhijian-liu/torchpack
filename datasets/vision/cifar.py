498f910571eb6caf9a32a7de2c0cfbe748e65ba4
Zhijian Liu <zhijianliu.cs@gmail.com>
2019-07-13 16:31:23 -0400

    [major] support cifar10/100 and imagenet10/100

--- datasets/vision/cifar.py
+++ datasets/vision/cifar.py
@@ -2,13 +2,20 @@ from torchvision import datasets, transforms
 
 from ..dataset import Dataset
 
-__all__ = ['CIFAR10']
+__all__ = ['CIFAR']
 
 
-class CIFAR10(Dataset):
-    def __init__(self, root, image_size):
+class CIFAR(Dataset):
+    def __init__(self, root, num_classes, image_size):
+        assert num_classes in [10, 100]
+
+        if num_classes == 10:
+            dataset = datasets.CIFAR10
+        else:
+            dataset = datasets.CIFAR100
+
         super().__init__({
-            'train': datasets.CIFAR10(
+            'train': dataset(
                 root=root, train=True, download=True,
                 transform=transforms.Compose([
                     transforms.RandomCrop(image_size, padding=4),
@@ -17,7 +24,7 @@ class CIFAR10(Dataset):
                     transforms.Normalize(mean=[0.4914, 0.4822, 0.4465], std=[0.2023, 0.1994, 0.2010]),
                 ])
             ),
-            'test': datasets.CIFAR10(
+            'test': dataset(
                 root=root, train=False, download=True,
                 transform=transforms.Compose([
                     transforms.Resize(image_size),
--- datasets/vision/imagenet.py
+++ datasets/vision/imagenet.py
@@ -2,14 +2,16 @@ import warnings
 
 from torchvision import datasets, transforms
 
+from ..dataset import Dataset
+
 __all__ = ['ImageNet']
 
 # filter warnings for corrupted data
 warnings.filterwarnings('ignore')
 
 
-class ImageNet(dict):
-    def __init__(self, root, image_size):
+class ImageNet(Dataset):
+    def __init__(self, root, num_classes, image_size):
         super().__init__({
             'train': datasets.ImageNet(
                 root=root, split='train', download=True,
@@ -30,3 +32,42 @@ class ImageNet(dict):
                 ])
             )
         })
+
+        classes_new = [None for _ in range(1000)]
+        for k in range(num_classes):
+            classes_new[(1000 // num_classes) * k] = k
+
+        for split in ['train', 'test']:
+            samples = []
+            for sample in self[split].samples:
+                if classes_new[sample[1]] is None:
+                    continue
+
+                samples.append((sample[0], classes_new[sample[1]]))
+            self[split].samples = samples
+
+        # for split in ['train', 'test']:
+        # print(self[split].classes)
+        # self.classes = classes
+        # print(self[split].class_to_idx)
+        # self.samples = samples
+        # print(self[split].samples)
+
+        # for split in ['train', 'test']:
+        #     print(self[split].targets)
+        # print(self[split].wnids)
+        # print(self[split].wnid_to_idx)
+
+    # for split in ['train', 'test']:
+    # wnid_to_classes = self._load_meta_file()[0]
+
+    # super(ImageNet, self).__init__(self.split_folder, **kwargs)
+    # self.root = root
+
+    # idcs = [idx for _, idx in self.imgs]
+    # self.wnids = self.classes
+    # self.wnid_to_idx = {wnid: idx for idx, wnid in zip(idcs, self.wnids)}
+    # self.classes = [wnid_to_classes[wnid] for wnid in self.wnids]
+    # self.class_to_idx = {cls: idx
+    #                      for clss, idx in zip(self.classes, idcs)
+    #                      for cls in clss}
