1da90fbf33d910448f963b7a5b9ea39d6224026c
Hanrui Wang <hanrui@mit.edu>
2019-07-17 22:02:18 -0400

    [minor] verify the mobilenet model and clean the code

--- models/vision/mobilenet.py
+++ models/vision/mobilenet.py
@@ -26,53 +26,6 @@ def conv_dw(input_channel, output_channel, stride):
     )
 
 
-class MobileNetref(nn.Module):
-    def __init__(self):
-        super(MobileNetref, self).__init__()
-
-        def conv_bn(inp, oup, stride):
-            return nn.Sequential(
-                nn.Conv2d(inp, oup, 3, stride, 1, bias=False),
-                nn.BatchNorm2d(oup),
-                nn.ReLU(inplace=True)
-            )
-
-        def conv_dw(inp, oup, stride):
-            return nn.Sequential(
-                nn.Conv2d(inp, inp, 3, stride, 1, groups=inp, bias=False),
-                nn.BatchNorm2d(inp),
-                nn.ReLU(inplace=True),
-
-                nn.Conv2d(inp, oup, 1, 1, 0, bias=False),
-                nn.BatchNorm2d(oup),
-                nn.ReLU(inplace=True),
-            )
-
-        self.feature = nn.Sequential(
-            conv_bn(3, 32, 2),
-            conv_dw(32, 64, 1),
-            conv_dw(64, 128, 2),
-            conv_dw(128, 128, 1),
-            conv_dw(128, 256, 2),
-            conv_dw(256, 256, 1),
-            conv_dw(256, 512, 2),
-            conv_dw(512, 512, 1),
-            conv_dw(512, 512, 1),
-            conv_dw(512, 512, 1),
-            conv_dw(512, 512, 1),
-            conv_dw(512, 512, 1),
-            conv_dw(512, 1024, 2),
-            conv_dw(1024, 1024, 1),
-            nn.AvgPool2d(7),
-        )
-        self.classifier = nn.Linear(1024, 1000)
-
-    def forward(self, x):
-        x = self.feature(x)
-        x = x.view(-1, 1024)
-        x = self.classifier(x)
-        return x
-
 class MobileNet(nn.Module):
     """
     MobileNet class
@@ -115,26 +68,11 @@ class MobileNet(nn.Module):
 
 if __name__ == '__main__':
     model = MobileNet(num_classes=1000)
-    model2 = MobileNetref()
     print(model)
 
     x = torch.randn(10, 3, 224, 224, device=torch.device('cpu'), dtype = torch.float)
-    torch.save(model.state_dict(), './model.pth')
-
-    model2.load_state_dict(torch.load('./model.pth'))
-    model2.eval()
 
     model.eval()
     y = model(x)
 
-    y2 = model2(x)
-
-    # y2.load(y.di)
-
-    # print(model.state_dict())
-    # print(model2.state_dict())
-
-    # print(min((y==y2).flatten()))
-
     print(y)
-    print(y2)
