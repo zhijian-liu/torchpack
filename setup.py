fc0382121ea4bbe9219f53592d4e6ec3ef089c26
Zhijian Liu <zhijianliu.cs@gmail.com>
2019-08-03 21:46:34 -0400

    [minor] support devices=*

--- setup.py
+++ setup.py
@@ -2,8 +2,11 @@ from setuptools import setup, find_packages
 
 from torchpack import __version__
 
-setup(name='torchpack',
-      version=__version__,
-      packages=find_packages(),
-      url='https://github.com/mit-han-lab/torchpack',
-      scripts=['bin/tprun'])
+setup(
+    name='torchpack',
+    version=__version__,
+    packages=find_packages(),
+    url='https://github.com/mit-han-lab/torchpack',
+    install_requires=['torch'],
+    scripts=['bin/tprun']
+)
--- torchpack/utils/device.py
+++ torchpack/utils/device.py
@@ -1,21 +1,26 @@
 import os
 
+import torch
+
 __all__ = ['set_cuda_visible_devices']
 
 
-def set_cuda_visible_devices(devs):
+def set_cuda_visible_devices(devs, env=os.environ):
     gpus = []
-    for dev in devs.split(','):
-        dev = dev.strip().lower()
-        if dev == 'cpu':
-            continue
-        if dev.startswith('gpu'):
-            dev = dev[3:]
-        if '-' in dev:
-            l, r = map(int, dev.split('-'))
-            gpus.extend(range(l, r + 1))
-        else:
-            gpus.append(int(dev))
-
-    os.environ['CUDA_VISIBLE_DEVICES'] = ','.join([str(gpu) for gpu in gpus])
+    if devs == '*':
+        gpus += range(torch.cuda.device_count())
+    else:
+        gpus = []
+        for dev in devs.split(','):
+            dev = dev.strip().lower()
+            if dev == 'cpu':
+                continue
+            if dev.startswith('gpu'):
+                dev = dev[3:]
+            if '-' in dev:
+                l, r = map(int, dev.split('-'))
+                gpus += range(l, r + 1)
+            else:
+                gpus += [int(dev)]
+        env['CUDA_VISIBLE_DEVICES'] = ','.join([str(gpu) for gpu in gpus])
     return gpus
