8c29583402380b6e21b1561c7de67b23fab741ea
Zhijian Liu <zhijianliu.cs@gmail.com>
2019-08-06 01:34:56 -0400

    [minor] update device

--- torchpack/utils/device.py
+++ torchpack/utils/device.py
@@ -2,25 +2,28 @@ import os
 
 import torch
 
-__all__ = ['set_cuda_visible_devices']
+__all__ = ['parse_devices', 'set_cuda_visible_devices']
 
 
-def set_cuda_visible_devices(devs, env=os.environ):
+def parse_devices(devs):
     gpus = []
     if devs == '*':
         gpus += range(torch.cuda.device_count())
     else:
-        gpus = []
         for dev in devs.split(','):
             dev = dev.strip().lower()
-            if dev == 'cpu':
-                continue
-            if dev.startswith('gpu'):
-                dev = dev[3:]
-            if '-' in dev:
-                l, r = map(int, dev.split('-'))
-                gpus += range(l, r + 1)
-            else:
-                gpus += [int(dev)]
-        env['CUDA_VISIBLE_DEVICES'] = ','.join([str(gpu) for gpu in gpus])
+            if dev != 'cpu':
+                if dev.startswith('gpu'):
+                    dev = dev[3:]
+                if '-' in dev:
+                    a, b = map(int, dev.split('-'))
+                    gpus += range(a, b + 1)
+                else:
+                    gpus += [int(dev)]
     return gpus
+
+
+def set_cuda_visible_devices(devs, env=os.environ):
+    devs = parse_devices(devs)
+    env['CUDA_VISIBLE_DEVICES'] = ','.join([str(dev) for dev in devs])
+    return devs
