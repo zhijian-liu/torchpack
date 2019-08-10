78ef54f00de968244096d19caa263beef2d5d68a
Zhijian Liu <zhijianliu.cs@gmail.com>
2019-08-10 16:42:19 -0400

    [minor] reformat code

--- torchpack/callbacks/group.py
+++ torchpack/callbacks/group.py
@@ -72,84 +72,12 @@ class MonitorGroup(Monitor):
         for monitor in monitors:
             assert isinstance(monitor, Monitor), type(monitor)
         self.monitors = monitors
+        self.scalars = defaultdict(list)
 
     def set_trainer(self, trainer):
         self.trainer = trainer
         for monitor in self.monitors:
             monitor.set_trainer(trainer)
-        self.scalars = defaultdict(list)
-
-    def add_summary(self, summary):
-        if isinstance(summary, six.binary_type):
-            summary = tf.Summary.FromString(summary)
-        assert isinstance(summary, tf.Summary), type(summary)
-
-        # TODO other types
-        for val in summary.value:
-            if val.WhichOneof('value') == 'simple_value':
-                val.tag = re.sub('tower[0-9]+/', '', val.tag)  # TODO move to subclasses
-
-                # TODO This hack is still needed, seem to disappear only when
-                # compiled from source.
-                suffix = '-summary'  # tensorflow#6150, tensorboard#59
-                if val.tag.endswith(suffix):
-                    val.tag = val.tag[:-len(suffix)]
-
-                for monitor in self.monitors:
-                    monitor.add_scalar(val.tag, val.simple_value)
-
-        for monitor in self.monitors:
-            monitor.add_summary(summary)
-
-    def add_scalar(self, name, val):
-        if isinstance(val, np.integer):
-            val = int(val)
-        if isinstance(val, np.floating):
-            val = float(val)
-
-        self.scalars[name].append((self.trainer.global_step, val))
-
-        summary = create_scalar_summary(name, val)
-        for monitor in self.monitors:
-            monitor.add_scalar(name, val)
-            monitor.add_summary(summary)
-
-    def add_image(self, name, val):
-        assert isinstance(val, np.ndarray)
-
-        im = val
-        if im.ndim == 4:
-            pass
-        elif im.ndim == 3:
-            if im.shape[-1] in [1, 3, 4]:
-                im = im[np.newaxis, ...]
-            else:
-                im = im[..., np.newaxis]
-        elif im.ndim == 2:
-            im = im[np.newaxis, :, :, np.newaxis]
-        else:
-            raise ValueError('Array of shape {} is not an image!'.format(im.shape))
-
-        summary = create_image_summary(name, im)
-        for monitor in self.monitors:
-            monitor.add_image(name, im)
-            monitor.add_summary(summary)
-
-    def add_event(self, event):
-        event.step = self.global_step
-        event.wall_time = time.time()
-        for monitor in self.monitors:
-            monitor.add_event(event)
-
-    def get_latest(self, name):
-        hist = self.scalars[name]
-        if len(hist) == 0:
-            raise KeyError(name)
-        else:
-            return hist[-1][1]
-
-    def get_history(self, name):
-        return self.scalars[name]
 
     def before_train(self):
         for monitor in self.monitors:
@@ -186,3 +114,75 @@ class MonitorGroup(Monitor):
     def trigger(self):
         for monitor in self.monitors:
             monitor.trigger()
+
+    def add_scalar(self, name, val):
+        if isinstance(val, np.integer):
+            val = int(val)
+        if isinstance(val, np.floating):
+            val = float(val)
+
+        self.scalars[name].append((self.trainer.global_step, val))
+
+        summary = create_scalar_summary(name, val)
+        for monitor in self.monitors:
+            monitor.add_scalar(name, val)
+            monitor.add_summary(summary)
+
+    def add_image(self, name, val):
+        assert isinstance(val, np.ndarray)
+
+        im = val
+        if im.ndim == 4:
+            pass
+        elif im.ndim == 3:
+            if im.shape[-1] in [1, 3, 4]:
+                im = im[np.newaxis, ...]
+            else:
+                im = im[..., np.newaxis]
+        elif im.ndim == 2:
+            im = im[np.newaxis, :, :, np.newaxis]
+        else:
+            raise ValueError('Array of shape {} is not an image!'.format(im.shape))
+
+        summary = create_image_summary(name, im)
+        for monitor in self.monitors:
+            monitor.add_image(name, im)
+            monitor.add_summary(summary)
+
+    def add_summary(self, summary):
+        if isinstance(summary, six.binary_type):
+            summary = tf.Summary.FromString(summary)
+        assert isinstance(summary, tf.Summary), type(summary)
+
+        # TODO other types
+        for val in summary.value:
+            if val.WhichOneof('value') == 'simple_value':
+                val.tag = re.sub('tower[0-9]+/', '', val.tag)  # TODO move to subclasses
+
+                # TODO This hack is still needed, seem to disappear only when
+                # compiled from source.
+                suffix = '-summary'  # tensorflow#6150, tensorboard#59
+                if val.tag.endswith(suffix):
+                    val.tag = val.tag[:-len(suffix)]
+
+                for monitor in self.monitors:
+                    monitor.add_scalar(val.tag, val.simple_value)
+
+        for monitor in self.monitors:
+            monitor.add_summary(summary)
+
+    def add_event(self, event):
+        event.step = self.global_step
+        event.wall_time = time.time()
+        for monitor in self.monitors:
+            monitor.add_event(event)
+
+    def get_latest(self, name):
+        hist = self.scalars[name]
+        if len(hist) == 0:
+            raise KeyError(name)
+        else:
+            return hist[-1][1]
+
+    def get_history(self, name):
+        return self.scalars[name]
