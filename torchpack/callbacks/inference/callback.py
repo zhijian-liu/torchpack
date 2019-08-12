38ec965be6e8981db4e483f7f9040d4226808150
Zhijian Liu <zhijianliu.cs@gmail.com>
2019-08-12 05:33:00 -0400

    [minor] update apis for `inference-callback`; add `inference-callbacks`

--- torchpack/callbacks/inference/callback.py
+++ torchpack/callbacks/inference/callback.py
@@ -2,7 +2,7 @@ from abc import ABCMeta
 
 import six
 
-__all__ = ['InferenceCallback']
+__all__ = ['InferenceCallback', 'InferenceCallbacks']
 
 
 @six.add_metaclass(ABCMeta)
@@ -13,27 +13,81 @@ class InferenceCallback(object):
 
     def set_trainer(self, trainer):
         self.trainer = trainer
+        self._set_trainer(trainer)
+
+    def _set_trainer(self, trainer):
+        pass
 
     def before_inference(self):
+        self._before_inference()
+
+    def _before_inference(self):
         """
         Called before inference starts.
         """
         pass
 
     def before_step(self, *args, **kwargs):
+        self._before_step(*args, **kwargs)
+
+    def _before_step(self, *args, **kwargs):
         """
         Called before every step.
         """
         pass
 
     def after_step(self, *args, **kwargs):
+        self._after_step(*args, **kwargs)
+
+    def _after_step(self, *args, **kwargs):
         """
         Called after every step.
         """
         pass
 
     def after_inference(self):
+        self._after_inference()
+
+    def _after_inference(self):
         """
         Called after inference ends.
         """
         pass
+
+
+@six.add_metaclass(ABCMeta)
+class InferenceCallbacks(InferenceCallback):
+    """
+    A container to hold inference callbacks.
+    """
+
+    def __init__(self, callbacks):
+        for callback in callbacks:
+            assert isinstance(callback, InferenceCallback), type(callback)
+        self.callbacks = callbacks
+
+    def _set_trainer(self, trainer):
+        for callback in self.callbacks:
+            callback.set_trainer(trainer)
+
+    def _before_inference(self):
+        for callback in self.callbacks:
+            callback.before_inference()
+
+    def _before_step(self, *args, **kwargs):
+        for callback in self.callbacks:
+            callback.before_step(*args, **kwargs)
+
+    def _after_step(self, *args, **kwargs):
+        for callback in self.callbacks:
+            callback.after_step(*args, **kwargs)
+
+    def _after_inference(self):
+        for callback in self.callbacks:
+            callback.after_inference()
+
+    def __len__(self):
+        return len(self.callbacks)
+
+    def __getitem__(self, index):
+        return self.callbacks[index]
