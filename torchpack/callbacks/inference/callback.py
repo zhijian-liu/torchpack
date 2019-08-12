11c3e5105681a8ba709ecbe20f6ad5c91a73c380
Zhijian Liu <zhijianliu.cs@gmail.com>
2019-08-11 23:06:03 -0400

    [minor] update api for inference-callback

--- torchpack/callbacks/inference/callback.py
+++ torchpack/callbacks/inference/callback.py
@@ -2,39 +2,38 @@ from abc import ABCMeta
 
 import six
 
-from torchpack.utils.logging import logger
-
 __all__ = ['InferenceCallback']
 
 
 @six.add_metaclass(ABCMeta)
 class InferenceCallback(object):
-    """ Base class of all inference callbacks.
+    """
+    Base class of inference callbacks.
     """
 
+    def set_trainer(self, trainer):
+        self.trainer = trainer
+
     def before_inference(self):
-        """ Called before a round of inference starts.
+        """
+        Called before inference starts.
         """
         pass
 
-    def trigger_epoch(self):
-        self.trigger()
+    def before_step(self, *args, **kwargs):
+        """
+        Called before every step.
+        """
+        pass
 
-    def trigger(self):
-        monitors = self.after_inference()
-        if monitors is None:
-            return
-        for k, v in monitors.items():
-            try:
-                v = float(v)
-            except ValueError:
-                logger.warn('{} returns a non-scalar statistics!'.format(type(self).__name__))
-                continue
-            else:
-                self.trainer.monitors.add_scalar(k, v)
+    def after_step(self, *args, **kwargs):
+        """
+        Called after every step.
+        """
+        pass
 
     def after_inference(self):
-        """ Called after a round of inference ends.
-        Returns a dict of scalar statistics which will be logged to monitors.
+        """
+        Called after inference ends.
         """
         pass
