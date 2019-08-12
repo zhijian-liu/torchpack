6c01f1bb9ecd9e774a7f3a8f7874993be8338988
Zhijian Liu <zhijianliu.cs@gmail.com>
2019-08-11 21:37:05 -0400

    [minor] reorder arguments

--- torchpack/callbacks/triggers.py
+++ torchpack/callbacks/triggers.py
@@ -8,18 +8,18 @@ class PeriodicTrigger(ProxyCallback):
     Trigger a callback every k global steps or every k epochs by its :meth:`trigger()` method.
     """
 
-    def __init__(self, callback, every_k_steps=None, every_k_epochs=None):
+    def __init__(self, callback, every_k_epochs=None, every_k_steps=None):
         """
         Args:
             callback (Callback): a Callback instance with a trigger method to be called.
-            every_k_steps (int): trigger when ``global_step % k == 0``.
             every_k_epochs (int): trigger when ``epoch_num % k == 0``.
+            every_k_steps (int): trigger when ``global_step % k == 0``.
         """
         assert isinstance(callback, Callback), type(callback)
         assert (every_k_epochs is not None) or (every_k_steps is not None), \
-            'every_k_steps and every_k_epochs cannot both be None!'
-        self.every_k_steps = every_k_steps
+            'every_k_epochs and every_k_steps cannot both be None!'
         self.every_k_epochs = every_k_epochs
+        self.every_k_steps = every_k_steps
         super().__init__(callback)
 
     def trigger_epoch(self):
@@ -87,29 +87,29 @@ class EnableCallbackIf(ProxyCallback):
 class PeriodicCallback(EnableCallbackIf):
     """
     The ``{before,after}_epoch``, ``{before,after}_run``, ``trigger_{epoch,step}``
-    methods of the given callback will be enabled only when ``global_step % every_k_steps == 0`
-    or ``epoch_num % every_k_epochs == 0``. The other methods are unaffected.
+    methods of the given callback will be enabled only when ``epoch_num % every_k_epochs == 0``
+    or ``global_step % every_k_steps == 0`. The other methods are unaffected.
     Note that this can only makes a callback **less** frequent than itself.
     """
 
-    def __init__(self, callback, every_k_steps=None, every_k_epochs=None):
+    def __init__(self, callback, every_k_epochs=None, every_k_steps=None):
         """
         Args:
             callback (Callback): a Callback instance.
-            every_k_steps (int): enable the callback when ``global_step % k == 0``.
             every_k_epochs (int): enable the callback when ``epoch_num % k == 0``.
+            every_k_steps (int): enable the callback when ``global_step % k == 0``.
         """
         assert isinstance(callback, Callback), type(callback)
         assert (every_k_epochs is not None) or (every_k_steps is not None), \
-            'every_k_steps and every_k_epochs cannot both be None!'
-        self.every_k_steps = every_k_steps
+            'every_k_epochs and every_k_steps cannot both be None!'
         self.every_k_epochs = every_k_epochs
+        self.every_k_steps = every_k_steps
         super().__init__(callback, PeriodicCallback.predicate)
 
     def predicate(self):
-        if self.every_k_steps is not None and self.trainer.global_step % self.every_k_steps == 0:
-            return True
         if self.every_k_epochs is not None and self.trainer.epoch_num % self.every_k_epochs == 0:
+            return True
+        if self.every_k_steps is not None and self.trainer.global_step % self.every_k_steps == 0:
             return True
         return False
 
