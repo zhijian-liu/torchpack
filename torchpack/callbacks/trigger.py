f56de97a379bfcf5258f9bcbaa2f6a9bb8c914da
Zhijian Liu <zhijianliu.cs@gmail.com>
2019-08-17 07:57:59 -0400

    [minor] reformat code; update docs

--- torchpack/callbacks/trigger.py
+++ torchpack/callbacks/trigger.py
@@ -5,7 +5,7 @@ __all__ = ['PeriodicTrigger', 'PeriodicCallback', 'EnableCallbackIf']
 
 class PeriodicTrigger(ProxyCallback):
     """
-    Trigger a callback every k global steps or every k epochs by its :meth:`trigger()` method.
+    Trigger the given callback every k steps or every k epochs.
     """
 
     def __init__(self, callback, every_k_epochs=None, every_k_steps=None):
@@ -15,23 +15,19 @@ class PeriodicTrigger(ProxyCallback):
             every_k_epochs (int): trigger when ``epoch_num % k == 0``.
             every_k_steps (int): trigger when ``global_step % k == 0``.
         """
+        super().__init__(callback)
         assert isinstance(callback, Callback), type(callback)
         assert (every_k_epochs is not None) or (every_k_steps is not None), \
-            'every_k_epochs and every_k_steps cannot both be None!'
+            '`every_k_epochs` and `every_k_steps` cannot both be None!'
         self.every_k_epochs = every_k_epochs
         self.every_k_steps = every_k_steps
-        super().__init__(callback)
-
-    def _trigger_epoch(self):
-        if self.every_k_epochs is None:
-            return
-        if self.trainer.epoch_num % self.every_k_epochs == 0:
-            self.callback.trigger()
 
     def _trigger_step(self):
-        if self.every_k_steps is None:
-            return
-        if self.trainer.global_step % self.every_k_steps == 0:
+        if self.every_k_steps is not None and self.trainer.global_step % self.every_k_steps == 0:
+            self.callback.trigger()
+
+    def _trigger_epoch(self):
+        if self.every_k_epochs is not None and self.trainer.epoch_num % self.every_k_epochs == 0:
             self.callback.trigger()
 
     def __str__(self):
@@ -40,45 +36,44 @@ class PeriodicTrigger(ProxyCallback):
 
 class EnableCallbackIf(ProxyCallback):
     """
-    Disable the ``{before,after}_epoch``, ``{before,after}_step``, ``trigger_{epoch,step}``
-    methods of a callback, unless some condition satisfies.
+    Disable `{before,after}_epoch`, `{before,after}_step`, `trigger_{epoch,step}` methods of
+    the given callback, unless some condition satisfies.
     """
 
     def __init__(self, callback, predicate):
         """
         Args:
             callback (Callback): a Callback instance.
-            predicate (self -> bool): a callable predicate. Has to be a pure function.
+            predicate (self -> bool): a callable predicate, which has to be a pure function.
                 The callback is disabled unless this predicate returns True.
         """
         super().__init__(callback)
+        assert isinstance(callback, Callback), type(callback)
         self.predicate = predicate
 
     def before_epoch(self):
-        self.enabled_epoch = self.predicate(self)
-        if self.enabled_epoch:
+        if self.predicate(self):
             super().before_epoch()
 
-    def after_epoch(self):
-        if self.enabled_epoch:
-            super().after_epoch()
-
     def before_step(self, *args, **kwargs):
-        self.enabled_step = self.predicate(self)
-        if self.enabled_step:
+        if self.predicate(self):
             super().before_step(*args, **kwargs)
 
     def after_step(self, *args, **kwargs):
-        if self.enabled_step:
+        if self.predicate(self):
             super().after_step(*args, **kwargs)
 
-    def trigger_epoch(self):
-        if self.enabled_epoch:
-            super().trigger_epoch()
-
     def trigger_step(self):
-        if self.enabled_step:
+        if self.predicate(self):
             super().trigger_step()
+
+    def after_epoch(self):
+        if self.predicate(self):
+            super().after_epoch()
+
+    def trigger_epoch(self):
+        if self.predicate(self):
+            super().trigger_epoch()
 
     def __str__(self):
         return 'EnableCallbackIf-' + str(self.callback)
@@ -86,10 +81,10 @@ class EnableCallbackIf(ProxyCallback):
 
 class PeriodicCallback(EnableCallbackIf):
     """
-    The ``{before,after}_epoch``, ``{before,after}_run``, ``trigger_{epoch,step}``
-    methods of the given callback will be enabled only when ``epoch_num % every_k_epochs == 0``
-    or ``global_step % every_k_steps == 0`. The other methods are unaffected.
-    Note that this can only makes a callback **less** frequent than itself.
+    The `{before,after}_epoch`, `{before,after}_run`, `trigger_{epoch,step}` methods of
+    the given callback will be enabled only when `epoch_num % every_k_epochs == 0` or
+    `global_step % every_k_steps == 0`. The other methods are unaffected.
+    Note that this can only makes a callback less frequent than itself.
     """
 
     def __init__(self, callback, every_k_epochs=None, every_k_steps=None):
@@ -99,12 +94,12 @@ class PeriodicCallback(EnableCallbackIf):
             every_k_epochs (int): enable the callback when ``epoch_num % k == 0``.
             every_k_steps (int): enable the callback when ``global_step % k == 0``.
         """
+        super().__init__(callback, PeriodicCallback.predicate)
         assert isinstance(callback, Callback), type(callback)
         assert (every_k_epochs is not None) or (every_k_steps is not None), \
-            'every_k_epochs and every_k_steps cannot both be None!'
+            '`every_k_epochs` and `every_k_steps` cannot both be None!'
         self.every_k_epochs = every_k_epochs
         self.every_k_steps = every_k_steps
-        super().__init__(callback, PeriodicCallback.predicate)
 
     def predicate(self):
         if self.every_k_epochs is not None and self.trainer.epoch_num % self.every_k_epochs == 0:
