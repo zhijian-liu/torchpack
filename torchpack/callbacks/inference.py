5545dded2a35d806dce3e9bc71842648258dbab9
Zhijian Liu <zhijianliu.cs@gmail.com>
2019-08-17 08:39:21 -0400

    [major] switch to `Callback` for `InferenceRunner`

--- torchpack/callbacks/inference.py
+++ torchpack/callbacks/inference.py
@@ -1,120 +1,29 @@
 import time
-from abc import ABCMeta
 
-import six
 import torch
 import tqdm
 from tensorpack.utils.utils import get_tqdm_kwargs
 from tensorpack.utils.utils import humanize_time_delta
 
-from torchpack.callbacks.callback import Callback
-from torchpack.cuda.copy import async_copy_to
+from torchpack.callbacks.callback import Callback, Callbacks
 from torchpack.utils.logging import logger
 
-__all__ = ['InferenceCallback', 'InferenceCallbacks', 'InferenceRunner']
-
-
-@six.add_metaclass(ABCMeta)
-class InferenceCallback(object):
-    """
-    Base class of inference callbacks.
-    """
-
-    def set_trainer(self, trainer):
-        self.trainer = trainer
-        self._set_trainer(trainer)
-
-    def _set_trainer(self, trainer):
-        pass
-
-    def before_inference(self):
-        self._before_inference()
-
-    def _before_inference(self):
-        """
-        Called before inference starts.
-        """
-        pass
-
-    def before_step(self, *args, **kwargs):
-        self._before_step(*args, **kwargs)
-
-    def _before_step(self, *args, **kwargs):
-        """
-        Called before every step.
-        """
-        pass
-
-    def after_step(self, *args, **kwargs):
-        self._after_step(*args, **kwargs)
-
-    def _after_step(self, *args, **kwargs):
-        """
-        Called after every step.
-        """
-        pass
-
-    def after_inference(self):
-        self._after_inference()
-
-    def _after_inference(self):
-        """
-        Called after inference ends.
-        """
-        pass
-
-
-@six.add_metaclass(ABCMeta)
-class InferenceCallbacks(InferenceCallback):
-    """
-    A container to hold inference callbacks.
-    """
-
-    def __init__(self, callbacks):
-        for callback in callbacks:
-            assert isinstance(callback, InferenceCallback), type(callback)
-        self.callbacks = callbacks
-
-    def _set_trainer(self, trainer):
-        for callback in self.callbacks:
-            callback.set_trainer(trainer)
-
-    def _before_inference(self):
-        for callback in self.callbacks:
-            callback.before_inference()
-
-    def _before_step(self, *args, **kwargs):
-        for callback in self.callbacks:
-            callback.before_step(*args, **kwargs)
-
-    def _after_step(self, *args, **kwargs):
-        for callback in self.callbacks:
-            callback.after_step(*args, **kwargs)
-
-    def _after_inference(self):
-        for callback in self.callbacks:
-            callback.after_inference()
-
-    def __len__(self):
-        return len(self.callbacks)
-
-    def __getitem__(self, index):
-        return self.callbacks[index]
+__all__ = ['InferenceRunner']
 
 
 class InferenceRunner(Callback):
     """
-    A callback that runs inference with a list of :class:`InferenceCallback`.
+    A callback that runs inference with a list of :class:`Callback`.
     """
 
     def __init__(self, dataflow, callbacks):
         for callback in callbacks:
-            assert isinstance(callback, InferenceCallback), callback
-        self.callbacks = callbacks
+            assert isinstance(callback, Callback), type(callback)
         self.dataflow = dataflow
+        self.callbacks = callbacks
 
     def _set_trainer(self, trainer):
-        self.callbacks = InferenceCallbacks(self.callbacks)
+        self.callbacks = Callbacks(self.callbacks)
         self.callbacks.set_trainer(trainer)
 
     def _trigger_epoch(self):
@@ -122,7 +31,7 @@ class InferenceRunner(Callback):
 
     def _trigger(self):
         start_time = time.time()
-        self.callbacks.before_inference()
+        self.callbacks.before_epoch()
 
         with torch.no_grad():
             for feed_dict in tqdm.tqdm(self.dataflow, **get_tqdm_kwargs()):
@@ -130,5 +39,5 @@ class InferenceRunner(Callback):
                 output_dict = self.trainer.run_step(feed_dict)
                 self.callbacks.after_step(feed_dict, output_dict)
 
-        self.callbacks.after_inference()
+        self.callbacks.after_epoch()
         logger.info('Inference finished in {}.'.format(humanize_time_delta(time.time() - start_time)))
--- torchpack/callbacks/metrics.py
+++ torchpack/callbacks/metrics.py
@@ -1,30 +1,30 @@
-from torchpack.callbacks.inference import InferenceCallback
+from torchpack.callbacks.callback import Callback
 
-__all__ = ['ClassificationAcc']
+__all__ = ['ClassificationError']
 
 
-class ClassificationAcc(InferenceCallback):
-    def __init__(self, k, logits_name='outputs', labels_name='targets', metric_name='validation_error'):
+class ClassificationError(Callback):
+    def __init__(self, k, logits_name='outputs', labels_name='targets', metric_name='acc/valid-top1'):
         self.k = k
         self.logits_name = logits_name
         self.labels_name = labels_name
         self.metric_name = metric_name
 
-    def _before_inference(self):
+    def _before_epoch(self):
         self.num_examples = 0
         self.num_correct = 0
 
     def _after_step(self, feed_dict, output_dict):
-        outputs = output_dict[self.logits_name]
-        targets = feed_dict[self.labels_name]
+        logits = output_dict[self.logits_name]
+        labels = feed_dict[self.labels_name]
 
-        _, indices = outputs.topk(self.k, 1, True, True)
+        _, indices = logits.topk(self.k, 1, True, True)
 
         indices = indices.transpose(0, 1)
-        masks = indices.eq(targets.view(1, -1).expand_as(indices))
+        masks = indices.eq(labels.view(1, -1).expand_as(indices))
 
-        self.num_examples += targets.size(0)
+        self.num_examples += labels.size(0)
         self.num_correct += masks[:self.k].view(-1).float().sum(0)
 
-    def _after_inference(self):
+    def _after_epoch(self):
         self.trainer.monitors.add_scalar(self.metric_name, self.num_correct / max(self.num_examples, 1) * 100.)
