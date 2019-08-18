3649106a3d70124de75b8a294bf5dc210b28b75e
Zhijian Liu <zhijianliu.cs@gmail.com>
2019-08-18 06:59:57 -0400

    [minor] update `setup.py`

--- setup.py
+++ setup.py
@@ -5,14 +5,15 @@ from torchpack import __version__
 setup(
     name='torchpack',
     version=__version__,
-    packages=find_packages(),
+    packages=find_packages(exclude=['examples', 'tests']),
     url='https://github.com/mit-han-lab/torchpack',
     install_requires=[
         'numpy>=1.14',
         'six',
-        'tqdm',
+        'tensorboardX>=1.8',
         'termcolor>=1.1',
         'torch>=1.2',
-        'torchvision>=0.4'
+        'torchvision>=0.4',
+        'tqdm>=4.31.0'
     ]
 )
