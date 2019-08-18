dc252ab7be9c4ec6398de4137225dcb359006f97
Zhijian Liu <zhijianliu.cs@gmail.com>
2019-08-18 03:21:59 -0400

    [minor] update `ClassificationError`

--- torchpack/callbacks/metrics.py
+++ torchpack/callbacks/metrics.py
@@ -4,27 +4,25 @@ __all__ = ['ClassificationError']
 
 
 class ClassificationError(Callback):
-    def __init__(self, k, logits_name='outputs', labels_name='targets', metric_name='acc/valid-top1'):
-        self.k = k
-        self.logits_name = logits_name
-        self.labels_name = labels_name
-        self.metric_name = metric_name
+    def __init__(self, topk=1, logits='outputs', labels='targets', name='accuracy'):
+        self.topk = topk
+        self.logits = logits
+        self.labels = labels
+        self.name = name
 
     def _before_epoch(self):
         self.num_examples = 0
         self.num_correct = 0
 
     def _after_step(self, feed_dict, output_dict):
-        logits = output_dict[self.logits_name]
-        labels = feed_dict[self.labels_name]
+        labels = feed_dict[self.labels]
+        logits = output_dict[self.logits]
 
-        _, indices = logits.topk(self.k, 1, True, True)
-
-        indices = indices.transpose(0, 1)
-        masks = indices.eq(labels.view(1, -1).expand_as(indices))
+        _, indices = logits.topk(self.topk, dim=1)
+        masks = indices.eq(labels.view(-1, 1).expand_as(indices))
 
         self.num_examples += labels.size(0)
-        self.num_correct += masks[:self.k].view(-1).float().sum(0)
+        self.num_correct += masks.sum().item()
 
     def _after_epoch(self):
-        self.trainer.monitors.add_scalar(self.metric_name, self.num_correct / max(self.num_examples, 1) * 100.)
+        self.trainer.monitors.add_scalar(self.name, self.num_correct / self.num_examples * 100.)
