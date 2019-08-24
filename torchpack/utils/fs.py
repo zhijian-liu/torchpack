53d373352ab0e8829f132ea767ac55dd83bb0b2d
zhijian <zhijianliu.cs@gmail.com>
2019-08-24 16:48:33 -0400

    [minor] rename `mkdir` as `makedir`; normalize path for `remove`

--- torchpack/utils/fs.py
+++ torchpack/utils/fs.py
@@ -2,16 +2,17 @@ import os
 import os.path as osp
 import shutil
 
-__all__ = ['mkdir', 'remove']
+__all__ = ['makedir', 'remove']
 
 
-def mkdir(path):
+def makedir(path):
     path = osp.normpath(path)
     os.makedirs(path, exist_ok=True)
     return path
 
 
 def remove(path):
+    path = osp.normpath(path)
     if osp.exists(path):
         if osp.isdir(path):
             return shutil.rmtree(path, ignore_errors=True)
