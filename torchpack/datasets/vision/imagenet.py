65df7f515008e577b3f4365f99cfc2f4a5c9a07a
Zhijian Liu <zhijianliu.cs@gmail.com>
2020-03-19 13:36:28 -0400

    [minor] support to set different custom transforms for different splits

--- torchpack/datasets/vision/imagenet.py
+++ torchpack/datasets/vision/imagenet.py
@@ -1,7 +1,7 @@
 import warnings
 
 import torchvision.datasets as datasets
-import torchvision.transforms as transforms
+from torchvision.transforms import *
 
 from torchpack.datasets.dataset import Dataset
 
@@ -21,36 +21,38 @@ class ImageNetDataset(datasets.ImageNet):
 
 
 class ImageNet(Dataset):
-    def __init__(self, root, num_classes=1000, image_size=224, transform=None):
+    def __init__(self, root, transforms=None, image_size=224,
+                 num_classes=1000):
+        if transforms is None:
+            transforms = dict()
+
+        if 'train' not in transforms:
+            transforms['train'] = Compose([
+                RandomResizedCrop(image_size),
+                RandomHorizontalFlip(),
+                ToTensor(),
+                Normalize(mean=[0.485, 0.456, 0.406],
+                          std=[0.229, 0.224, 0.225])
+            ])
+
+        if 'test' not in transforms:
+            transforms['test'] = Compose([
+                Resize(int(image_size / 0.875)),
+                CenterCrop(image_size),
+                ToTensor(),
+                Normalize(mean=[0.485, 0.456, 0.406],
+                          std=[0.229, 0.224, 0.225])
+            ])
+
         super().__init__({
             'train':
-            ImageNetDataset(
-                root=root,
-                split='train',
-                transform=transform or transforms.Compose([
-                    transforms.RandomResizedCrop(image_size),
-                    transforms.RandomHorizontalFlip(),
-                    transforms.ToTensor(),
-                    transforms.Normalize(
-                        mean=[0.485, 0.456, 0.406],
-                        std=[0.229, 0.224, 0.225],
-                    )
-                ]),
-            ),
+            ImageNetDataset(root=root,
+                            split='train',
+                            transform=transforms['train']),
             'test':
-            ImageNetDataset(
-                root=root,
-                split='val',
-                transform=transform or transforms.Compose([
-                    transforms.Resize(int(image_size / 0.875)),
-                    transforms.CenterCrop(image_size),
-                    transforms.ToTensor(),
-                    transforms.Normalize(
-                        mean=[0.485, 0.456, 0.406],
-                        std=[0.229, 0.224, 0.225],
-                    )
-                ]),
-            )
+            ImageNetDataset(root=root,
+                            split='val',
+                            transform=transforms['test'])
         })
 
         # sample classes by strided indexing
