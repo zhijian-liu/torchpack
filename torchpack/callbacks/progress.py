68372d5d12e1ac17d15ef8b0be21df5fe735ac77
Zhijian Liu <zhijianliu.cs@gmail.com>
2020-03-19 22:03:03 -0400

    [minor] reformat progress

--- torchpack/callbacks/progress.py
+++ torchpack/callbacks/progress.py
@@ -3,11 +3,10 @@ from collections import deque
 
 import numpy as np
 import tqdm
-from tensorpack.utils.utils import get_tqdm_kwargs
-from tensorpack.utils.utils import humanize_time_delta
+from tensorpack.utils.utils import get_tqdm_kwargs, humanize_time_delta
 
 from torchpack.callbacks.callback import Callback
-from torchpack.utils.logging import logger
+from torchpack.logging import logger
 from torchpack.utils.matching import IENameMatcher
 
 __all__ = ['ProgressBar', 'EstimatedTimeLeft']
@@ -21,21 +20,23 @@ class ProgressBar(Callback):
 
     master_only = True
 
-    def __init__(self, include='*', exclude=None):
-        self.matcher = IENameMatcher(include, exclude)
+    def __init__(self, includes='*', excludes=None):
+        self.matcher = IENameMatcher(includes, excludes)
 
     def _before_epoch(self):
-        self.pbar = tqdm.trange(self.trainer.steps_per_epoch, **get_tqdm_kwargs())
+        self.pbar = tqdm.trange(self.trainer.steps_per_epoch,
+                                **get_tqdm_kwargs())
 
     def _trigger_step(self):
         texts = []
         for name, (step, scalar) in sorted(self.trainer.monitors.items()):
-            if step == self.trainer.global_step and isinstance(scalar, (int, float)) and self.matcher.match(name):
+            if step == self.trainer.global_step and \
+                    isinstance(scalar, (int, float)) and \
+                    self.matcher.match(name):
                 texts.append('[{}] = {:.3g}'.format(name, scalar))
         if texts:
             self.pbar.set_description(', '.join(texts))
         self.pbar.update()
-        # print('\r' + ', '.join(texts), end='')
 
     def _after_epoch(self):
         self.pbar.close()
@@ -64,6 +65,9 @@ class EstimatedTimeLeft(Callback):
         self.times.append(time.time() - self.last_time)
         self.last_time = time.time()
 
-        epoch_time = np.median(self.times) if self.median else np.mean(self.times)
-        time_left = (self.trainer.max_epoch - self.trainer.epoch_num) * epoch_time
-        logger.info('Estimated time left: {}.'.format(humanize_time_delta(time_left)))
+        epoch_time = np.median(self.times) if self.median else \
+                     np.mean(self.times)
+        time_left = (self.trainer.max_epoch - self.trainer.epoch_num) \
+                    * epoch_time
+        logger.info('Estimated time left: {}.'.format(
+            humanize_time_delta(time_left)))
