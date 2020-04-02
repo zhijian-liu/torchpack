c9cde026cee34e1f482a82c0dad6366ae335fb35
Zhijian Liu <zhijianliu.cs@gmail.com>
2020-04-02 15:36:44 -0400

    [minor] rename `SetDeviceAction` to `SetDevicesAction`

--- torchpack/utils/argparse.py
+++ torchpack/utils/argparse.py
@@ -6,7 +6,7 @@ __all__ = ['ArgumentParser']
 
 
 # from https://github.com/vacancy/Jacinle/blob/master/jacinle/cli/argument.py
-class SetDevicesAction(argparse.Action):
+class SetDeviceAction(argparse.Action):
     def __init__(self,
                  option_strings,
                  dest,
@@ -36,4 +36,4 @@ class SetDevicesAction(argparse.Action):
 class ArgumentParser(argparse.ArgumentParser):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.register('action', 'set_devices', SetDevicesAction)
+        self.register('action', 'set_device', SetDeviceAction)
