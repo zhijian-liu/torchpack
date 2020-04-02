90833072595c30c7b7892246bc502c2c2c216cd6
Zhijian Liu <zhijianliu.cs@gmail.com>
2020-04-02 16:44:47 -0400

    [minor] reformat code

--- torchpack/environ/dir.py
+++ torchpack/environ/dir.py
@@ -14,9 +14,10 @@ def get_run_dir():
     return _run_dir
 
 
-def set_run_dir(dirname):
+def set_run_dir(dirpath):
     global _run_dir
-    _run_dir = fs.makedir(dirname)
+    _run_dir = osp.normpath(dirpath)
+    fs.makedir(_run_dir)
     set_logger_dir(osp.join(_run_dir, 'logging'))
 
 
@@ -24,17 +25,8 @@ def get_logger_dir():
     return _logger_dir
 
 
-def set_logger_dir(dirname):
+def set_logger_dir(dirpath):
     global _logger_dir
-    _logger_dir = fs.makedir(dirname)
+    _logger_dir = osp.normpath(dirpath)
+    fs.makedir(_logger_dir)
     set_handler(osp.join(_logger_dir, time.strftime('%Y%m%d-%H%M%S') + '.log'))
-
-
-# TODO: implement auto_set_dir
-# def auto_set_dir(name=None):
-#     mod = sys.modules['__main__']
-#     basename = os.path.basename(mod.__file__)
-#     auto_dirname = os.path.join('runs', basename[:basename.rfind('.')])
-#     if name:
-#         auto_dirname += '_%s' % name if os.name == 'nt' else ':%s' % name
-#     set_logger_dir(auto_dirname)
