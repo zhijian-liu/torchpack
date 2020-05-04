f5e26a3a476bb03247dee7b39d3beb24154a1e4e
Zhijian Liu <zhijianliu.cs@gmail.com>
2020-05-04 17:14:18 -0400

    [minor] add `batched_index_select` to `nn.functional`

---
+++ torchpack/nn/functional/index.py
@@ -0,0 +1,21 @@
+import torch
+
+__all__ = ['batched_index_select']
+
+
+def batched_index_select(inputs, indices, dim):
+    vsizes, esizes = [], []
+    for k, size in enumerate(inputs.shape):
+        if k == 0:
+            vsizes.append(size)
+            esizes.append(-1)
+        elif k == dim:
+            vsizes.append(-1)
+            esizes.append(-1)
+        else:
+            vsizes.append(1)
+            esizes.append(size)
+
+    indices = indices.view(vsizes).expand(esizes)
+    outputs = torch.gather(inputs, dim, indices)
+    return outputs
