a5fde2fe9849662d604741b19907f2bf4179669e
Zhijian Liu <zhijianliu.cs@gmail.com>
2020-06-19 23:07:14 -0400

    [minor] update `logger` to `loguru`

--- torchpack/environ/dir.py
+++ torchpack/environ/dir.py
@@ -1,13 +1,12 @@
 import os.path as osp
 import time
 
-from ..logging import set_handler
+from ..logging import logger
 from ..utils import fs
 
-__all__ = ['get_run_dir', 'set_run_dir', 'get_logger_dir', 'set_logger_dir']
+__all__ = ['get_run_dir', 'set_run_dir']
 
 _run_dir = None
-_logger_dir = None
 
 
 def get_run_dir():
@@ -18,15 +17,6 @@ def set_run_dir(dirpath):
     global _run_dir
     _run_dir = osp.normpath(dirpath)
     fs.makedir(_run_dir)
-    set_logger_dir(osp.join(_run_dir, 'logging'))
-
-
-def get_logger_dir():
-    return _logger_dir
-
-
-def set_logger_dir(dirpath):
-    global _logger_dir
-    _logger_dir = osp.normpath(dirpath)
-    fs.makedir(_logger_dir)
-    set_handler(osp.join(_logger_dir, time.strftime('%Y%m%d-%H%M%S') + '.log'))
+    logger.add(
+        osp.join(_run_dir, 'logging',
+                 time.strftime('%Y%m%d-%H%M%S') + '.log'))
--- torchpack/logging/logger.py
+++ torchpack/logging/logger.py
@@ -1,58 +1,11 @@
-import logging
-import os
 import sys
-import time
 
-from termcolor import colored
+from loguru import logger
 
-from .formatter import Formatter
+__all__ = ['logger']
 
-__all__ = ['logger', 'get_logger', 'set_handler', 'get_level', 'set_level']
-
-_loggers = []
-_handler = None
-_level = logging.INFO
-
-
-def get_logger(name=None, formatter=Formatter):
-    logger = logging.getLogger(name)
-    _loggers.append(logger)
-
-    logger.propagate = False
-    logger.setLevel(_level)
-
-    handler = logging.StreamHandler(stream=sys.stdout)
-    handler.setFormatter(formatter(datefmt='%m/%d %H:%M:%S'))
-    logger.addHandler(handler)
-    return logger
-
-
-def set_handler(filename):
-    handler = logging.FileHandler(filename=filename,
-                                  encoding='utf-8',
-                                  mode='w')
-    handler.setFormatter(Formatter(datefmt='%m/%d %H:%M:%S'))
-
-    global _handler
-    if _handler:
-        for logger in _loggers:
-            logger.removeHandler(_handler)
-        del _handler
-
-    _handler = handler
-    for logger in _loggers:
-        logger.addHandler(handler)
-
-
-def get_level():
-    return _level
-
-
-def set_level(level):
-    global _level
-    _level = level
-    for logger in _loggers:
-        logger.setLevel(level)
-
-
-logger = get_logger('torchpack')
+logger.remove()
+logger.add(
+    sys.stderr,
+    format=
+    '<green>[{time:YYYY-MM-DD HH:mm:ss.SSS}]</green> <level>{message}</level>')
