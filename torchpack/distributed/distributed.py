c0a4a846064700f0fc031d048aa47daf445b7ade
Zhijian Liu <zhijianliu.cs@gmail.com>
2020-06-22 13:36:30 -0400

    [minor] add `local_size` and `local_rank`

--- torchpack/distributed/distributed.py
+++ torchpack/distributed/distributed.py
@@ -5,7 +5,7 @@ import time
 import torch
 import torch.distributed as dist
 
-__all__ = ['init', 'size', 'rank', 'is_master']
+__all__ = ['init', 'size', 'rank', 'local_size', 'local_rank', 'is_master']
 
 
 def init():
@@ -22,6 +22,14 @@ def rank():
     if not dist.is_available() or not dist.is_initialized():
         return 0
     return dist.get_rank()
+
+
+def local_size():
+    return os.environ.get('LOCAL_SIZE', 1)
+
+
+def local_rank():
+    return os.environ.get('LOCAL_RANK', 0)
 
 
 def is_master():
