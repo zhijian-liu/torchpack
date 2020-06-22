0483b161783f7ddee87e3da492ffc0add5b43413
Zhijian Liu <zhijianliu.cs@gmail.com>
2020-06-22 13:51:48 -0400

    [minor] switch `init_method` from `env://` to `tcp://`

--- torchpack/distributed/distributed.py
+++ torchpack/distributed/distributed.py
@@ -9,19 +9,20 @@ __all__ = ['init', 'size', 'rank', 'local_size', 'local_rank', 'is_master']
 
 
 def init():
-    dist.init_process_group(backend='nccl')
+    dist.init_process_group(backend='nccl',
+                            init_method='tcp://' +
+                            os.environ.get('MASTER_ADDR') + ':' +
+                            os.environ.get('MASTER_PORT'),
+                            world_size=size(),
+                            rank=rank())
 
 
 def size():
-    if not dist.is_available() or not dist.is_initialized():
-        return 1
-    return dist.get_world_size()
+    return os.environ.get('WORLD_SIZE', 1)
 
 
 def rank():
-    if not dist.is_available() or not dist.is_initialized():
-        return 0
-    return dist.get_rank()
+    return os.environ.get('WORLD_RANK', 0)
 
 
 def local_size():
