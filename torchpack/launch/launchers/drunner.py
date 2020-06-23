4faa593c98dffdfd133c4b577bbf584c6c52f382
Zhijian Liu <zhijianliu.cs@gmail.com>
2020-06-23 19:40:16 -0400

    [minor] rename files

--- torchpack/launch/launchers/drunner.py
+++ torchpack/launch/launchers/drunner.py
@@ -5,7 +5,7 @@ import socket
 import sys
 from shlex import quote
 
-__all__ = ['launch']
+__all__ = ['main']
 
 
 def get_free_tcp_port():
@@ -15,7 +15,7 @@ def get_free_tcp_port():
     return port
 
 
-def launch():
+def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
         '-np',
