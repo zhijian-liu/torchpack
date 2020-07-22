dbccd59ca4a262db4c4794613016c112e56f4ba7
Zhijian Liu <zhijianliu.cs@gmail.com>
2020-07-22 14:51:38 -0400

    [minor] update examples with (incomplete) type annotations

--- examples/image-classification/train.py
+++ examples/image-classification/train.py
@@ -1,26 +1,26 @@
-import os.path as osp
+import os
 import sys
 
 import torch
-import torch.nn as nn
-import torchpack.distributed as dist
+from torch import nn
+
+from torchpack import distributed as dist
 from torchpack.callbacks import (InferenceRunner, MaxSaver, Saver,
                                  SaverRestore, TopKCategoricalAccuracy)
 from torchpack.datasets.vision import ImageNet
 from torchpack.environ import set_run_dir
 from torchpack.models.vision import MobileNetV2
 from torchpack.utils.logging import logger
-
 from utils.trainer import ClassificationTrainer
 
 
-def main():
+def main() -> None:
     dist.init()
 
     torch.backends.cudnn.benchmark = True
     torch.cuda.set_device(dist.local_rank())
 
-    set_run_dir(osp.join('runs', 'imagenet100.mobilenetv2'))
+    set_run_dir(os.path.join('runs', 'imagenet100.mobilenetv2'))
     logger.info(' '.join([sys.executable] + sys.argv))
 
     logger.info('Loading the dataset.')
@@ -59,13 +59,13 @@ def main():
         max_epoch=150,
         callbacks=[
             SaverRestore(),
-            Saver(),
             InferenceRunner(dataflow['test'],
                             callbacks=[
                                 TopKCategoricalAccuracy(k=1, name='acc/top1'),
                                 TopKCategoricalAccuracy(k=5, name='acc/top5')
                             ]),
-            MaxSaver('acc/top1')
+            MaxSaver('acc/top1'),
+            Saver()
         ])
 
 
--- examples/image-classification/utils/trainer.py
+++ examples/image-classification/utils/trainer.py
@@ -1,25 +1,27 @@
-from torchpack.engine import Trainer
+from typing import Any, Dict
+
+from torchpack.train import Trainer
 
 __all__ = ['ClassificationTrainer']
 
 
 class ClassificationTrainer(Trainer):
-    def __init__(self, *, model, criterion, optimizer, scheduler):
+    def __init__(self, *, model, criterion, optimizer, scheduler) -> None:
         self.model = model
         self.criterion = criterion
         self.optimizer = optimizer
         self.scheduler = scheduler
 
-    def _before_epoch(self):
+    def _before_epoch(self) -> None:
         self.model.train()
 
-    def _run_step(self, feed_dict):
+    def _run_step(self, feed_dict: Dict[str, Any]) -> Dict[str, Any]:
         inputs = feed_dict['image'].cuda(non_blocking=True)
         targets = feed_dict['class'].cuda(non_blocking=True)
 
         outputs = self.model(inputs)
 
-        if self.model.training:
+        if outputs.requires_grad:
             loss = self.criterion(outputs, targets)
             self.summary.add_scalar('loss', loss.item())
 
@@ -29,18 +31,18 @@ class ClassificationTrainer(Trainer):
 
         return {'outputs': outputs, 'targets': targets}
 
-    def _after_epoch(self):
+    def _after_epoch(self) -> None:
         self.model.eval()
         self.scheduler.step()
 
-    def _state_dict(self):
-        return {
-            'model': self.model.state_dict(),
-            'optimizer': self.optimizer.state_dict(),
-            'scheduler': self.scheduler.state_dict()
-        }
+    def _state_dict(self) -> Dict[str, Any]:
+        state_dict = dict()
+        state_dict['model'] = self.model.state_dict()
+        state_dict['optimizer'] = self.optimizer.state_dict()
+        state_dict['scheduler'] = self.scheduler.state_dict()
+        return state_dict
 
-    def _load_state_dict(self, state_dict):
-        self.model.load_state_dict(state_dict['model'])
-        self.optimizer.load_state_dict(state_dict['optimizer'])
-        self.scheduler.load_state_dict(state_dict['scheduler'])
+    def _load_state_dict(self, state_dict: Dict[str, Any]) -> None:
+        self.model.load_state_dict(state_dict.pop('model'))
+        self.optimizer.load_state_dict(state_dict.pop('optimizer'))
+        self.scheduler.load_state_dict(state_dict.pop('scheduler'))
