727ddbcac9e444762fb122e4e827370758dce5fb
Zhijian Liu <zhijianliu.cs@gmail.com>
2020-07-29 10:02:12 -0400

    [minor] rename `names` to `tags`

--- torchpack/environ/rundir.py
+++ torchpack/environ/rundir.py
@@ -28,11 +28,11 @@ def set_run_dir(dirpath: str) -> None:
 
 
 def auto_set_run_dir() -> str:
-    names = ['run']
+    tags = ['run']
     if git.is_inside_work_tree():
-        names.append(git.get_commit_hash()[:8])
+        tags.append(git.get_commit_hash()[:8])
     if configs:
-        names.append(configs.hash()[:8])
-    run_dir = os.path.join('runs', '-'.join(names))
+        tags.append(configs.hash()[:8])
+    run_dir = os.path.join('runs', '-'.join(tags))
     set_run_dir(run_dir)
     return run_dir
